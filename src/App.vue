f15037ea2728ad75014a9e135d830569b94be640
Egorov Ivan <HD-74@yandex.ru>
2023-02-05 13:19:47 +0600

    add aliases, test class-components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="test">Let`s vue</div>
+    <div>
+        <AppHeader />
+        <div class="test">{{ greetings }}</div>
+    </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import AppHeader from "@components/app-header.vue";
+
+@Component({
+    components: {
+        AppHeader
+    }
+})
+
+export default class App extends Vue {
+
+    greetings = "Hello, let`s vue"
+}
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 </style>
---
+++ src/components/app-header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>{{ headerTitle }}</header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from 'vue-class-component'
+
+@Component
+export default class AppHeader extends Vue {
+    headerTitle = "header-title"
+}
+</script>
+
+<style scoped>
+
+</style>
